da8eef0a0d38c98261240aacd58d58e9122e167e
User <s1940009@lamp.ms.wits.ac.za>
2020-02-27 19:40:00 +0200

    Removed the ID attribute and modified the attribute names to start with the table name in order for us to be able to tell where the attribute comes from just by looking at its name

--- Users.sql
+++ Users.sql
@@ -1,5 +1,5 @@
 
-create table USERS (ID int primary key auto_increment, USERNAME varchar(20) not null unique, 
-FNAME varchar(100) not null, LNAME varchar(50) not null, 
-PASSWORD longtext not null, STUDENT_NO varchar(20) not null, 
-ICAM_No varchar(50) not null, CONTACT_NO char(10) not null,EMAIL_ADDRESS varchar(100) not null);
+create table USERS (USERS_USERNAME varchar(20) primary key, 
+USERS_FNAME varchar(100) not null, USERS_LNAME varchar(50) not null, 
+USERS_PASSWORD longtext not null, USERS_STUDENT_NO varchar(20) not null, 
+USERS_ICAM_No varchar(50) not null, USERS_CONTACT_NO char(10) not null,USERS_EMAIL_ADDRESS varchar(100) not null);
