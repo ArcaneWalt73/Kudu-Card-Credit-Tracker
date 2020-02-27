68d3b7a43a50fa04bc98316447b49bfcd315bc53
User <s1876473@lamp.ms.wits.ac.za>
2020-02-27 21:29:28 +0200

    updated this script to match changes in the USERS table

--- forgetPassword.php
+++ forgetPassword.php
@@ -5,8 +5,7 @@ $database = "d1876473";
 $link = mysqli_connect("127.0.0.1",$username,$password,$database);
 $username1 = $_POST["userName"];
 $password1 = $_POST["newPassword"];
-
-if($result=mysqli_query($link,"update USERS set USERS_PASSWORD='$password1' where USERS_USERNAME ='$username1'"));
+if($result=mysqli_query($link,"UPDATE USERS SET USERS_PASSWORD='$password1' WHERE USERS_USERNAME='$username1'"));
 echo json_encode($result);
 mysqli_close($link);
 ?>
