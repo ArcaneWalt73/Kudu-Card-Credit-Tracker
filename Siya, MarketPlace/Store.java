ad8fa2c577de1bdd158fb909f8477c816f1e3f5d
Siyabonga Hlomuka <1384685@students.wits.ac.za>
2020-03-04 16:16:03 +0200

    Marketplace java files

---
+++ Siya, MarketPlace/Store.java
@@ -0,0 +1,29 @@
+
+
+import java.util.ArrayList;
+
+public class Store {
+	 private String name;
+	    private ArrayList<Item> Items = new ArrayList<>();
+
+	    Store(String storeName ){
+	        this.name = storeName;
+	    }
+
+	    public String getName(){
+	        return this.name;
+	    }
+
+	    public void addItem(String Item_name, int Item_price){
+	        Item StoreItem = new Item(Item_name , Item_price);
+	        Items.add(StoreItem);
+	    }
+
+	    public Item getItem(int Index_of_item){
+	        return Items.get(Index_of_item);
+	    }
+
+	    public int get_Number_of_Items(){
+	        return Items.size();
+	    }
+}
