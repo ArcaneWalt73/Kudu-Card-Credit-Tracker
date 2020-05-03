91951a1fb8a24f185fbd685ba02b6415b8ecf4e9
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-05-03 23:08:48 +0200

    Update login.php
    changed the php file to link to the new database

--- login.php
+++ login.php
@@ -1,13 +1,14 @@
 <?php
-$username =  "tshego";//The username for mysql
-$password = "password123";
-$database = "kudu_card_tracker";
+$username =  "s1965919";//The username for mysql
+$password = "ICTPass1670";
+$database = "d1965919";
 $link = mysqli_connect("127.0.0.1", $username, $password, $database);
 $output = array();
 $studentNo = $_POST["studentNumber"]; //The username of the person who is trying to login
 $password1 = $_POST["password"]
-if ($result = mysqli_query($link, "select * from STUDENTS where STUDENT_NO='$studentNo'")){
-	if(password_verify($password,$result)){
+if ($result = mysqli_query($link, "select USERS_PASSWORD from USERS where USERS_STUDENT_NO='$studentNo';")){
+	$hashed = $result->fetch_assoc()['USERS_PASSWORD'];
+	if(!(empty($password1)) && password_verify($password1,$hashed)){
 		echo json_encode("true");
 	}
 	else{
