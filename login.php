83c50f2aeb87160236ff8cfab3a0b87a1b315752
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-05-24 17:20:41 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+$username =  "s1965919"; //The username for the database
+$password = "ICTPass1670";
+$database = "d1965919";
+$link = mysqli_connect("127.0.0.1", $username, $password, $database);
+$output = array();
+$studentNo = $_POST["studentNumber"]; //The username of the person who is trying to login
+$password1 = $_POST["password"];
+
+if ($result = mysqli_query($link, "select USERS_PASSWORD from USERS where USERS_STUDENT_NO='$studentNo';")){
+        $hashed = $result->fetch_assoc()['USERS_PASSWORD'];
+        if(!(empty($password1)) && password_verify($password1,$hashed)){
+                echo json_encode("true");
+        }
+        else{
+                echo json_encode("false");
+        }
+        mysqli_close($link);
+}
+?>
