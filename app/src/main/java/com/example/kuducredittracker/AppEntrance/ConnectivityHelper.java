0a121c73e0098c2bca6361aa448d0fff0d5a597f
Ch35tEr <48945081+Ch35tEr@users.noreply.github.com>
2020-06-12 23:40:18 +0200

    Create ConnectivityHelper.java

---
+++ app/src/main/java/com/example/kuducredittracker/AppEntrance/ConnectivityHelper.java
@@ -0,0 +1,38 @@
+package com.example.kuducredittracker.AppEntrance;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class ConnectivityHelper {
+    // TCP/HTTP/DNS (depending on the port, 53=DNS, 80=HTTP, etc.)
+    Context context;
+
+    public ConnectivityHelper(Context c) {
+        context = c;
+    }
+
+    public boolean isOnline() {
+        try {
+            int timeoutMs = 1500;
+            Socket sock = new Socket();
+            SocketAddress sockaddr = new InetSocketAddress("8.8.8.8", 53);
+
+            sock.connect(sockaddr, timeoutMs);
+            sock.close();
+
+            return true;
+        } catch (IOException e) { return false; }
+    }
+
+    public boolean isOnline_Second() {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+}
