5e20969a911bc06fa12ec0760a864bfab5c2ed37
Ch35tEr <48945081+Ch35tEr@users.noreply.github.com>
2020-06-13 19:15:30 +0200

    Update ConnectivityHelper.java

--- app/src/main/java/com/example/kuducredittracker/AppEntrance/ConnectivityHelper.java
+++ app/src/main/java/com/example/kuducredittracker/AppEntrance/ConnectivityHelper.java
@@ -35,4 +35,17 @@ public class ConnectivityHelper {
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
+
+    public static boolean checkInternetConnection(Context context) {
+        ConnectivityManager con_manager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (con_manager.getActiveNetworkInfo() != null
+                && con_manager.getActiveNetworkInfo().isAvailable()
+                && con_manager.getActiveNetworkInfo().isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
